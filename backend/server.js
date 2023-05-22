3f2f82387d69128122b8e99c6840e6006294bb87
fangqiank <fangqiank@msn.com>
2023-05-22 10:53:16 +0800

    fix error 10

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,11 @@ app.use('/api/users', userRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/.up.railway.app/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
   app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, '.up.railway.app', 'frontend', 'dist', 'index.html'))
-	)
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
