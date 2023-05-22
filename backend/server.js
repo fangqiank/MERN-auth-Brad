a57596d2fb4b422a0d025d8984ea093bf060e519
fangqiank <fangqiank@msn.com>
2023-05-22 09:24:33 +0800

    fix error 4

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
 import {notFound, errorHandler} from './middelware/errorMiddleware.js'
 import {connectDB} from './config/db.js'
-
-dotenv.config()
 
 const port = process.env.PORT || 5000
 
