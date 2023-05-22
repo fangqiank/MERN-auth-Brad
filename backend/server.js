6866a086fd8a996beb72594bde077df396113f30
fangqiank <fangqiank@msn.com>
2023-05-22 10:12:12 +0800

    fix error 10

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('/frontend/dist'))
   app.get('*', (req, res) =>
     // res.sendFile(path.resolve(__dirname, '.up.railway.app', 'frontend', 'dist', 'index.html'))
+		console.log(path.resolve('frontend', 'dist', 'index.html')),
 		res.sendFile(path.resolve('frontend', 'dist', 'index.html'))
   )
 } else {
